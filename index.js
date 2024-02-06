846c5fc573d7b9fd7c5284809e50da0f7216f7b7
jvmo <jvmodeveloper@gmail.com>
2024-02-06 02:49:54 -0500

    index.js

--- index.js
+++ index.js
@@ -18,33 +18,33 @@ function IntercomDoor(log, config, api) {
   this.voltagePin = config.voltagePin || 17; // the GPIO pin for the voltage measurement
   this.apiURL = config.apiURL || "http://localhost:8080"; // the URL of the REST API server
 
-  // get the HAP object from the API
-  const { Accessory, Service, Characteristic } = this.api.hap;
-
   // initialize the GPIO pins using the onoff library
   const Gpio = require("onoff").Gpio;
   this.relay = new Gpio(this.relayPin, "out"); // set the relay pin as an output
   this.voltage = new Gpio(this.voltagePin, "in", "both"); // set the voltage pin as an input with both edge detection
 
   // create a new accessory with the information service
-  this.accessory = new Accessory(this.name, uuid.v4()); // updated line
-  this.informationService = this.accessory.getService(Service.AccessoryInformation);
+  this.accessory = new this.api.hap.Accessory(this.name, uuid.v4()); // use api.hap.Accessory
+  this.informationService = this.accessory.getService(this.api.hap.Service.AccessoryInformation);
   this.informationService
-    .setCharacteristic(Characteristic.Manufacturer, "jvmo")
-    .setCharacteristic(Characteristic.Model, "Intercom Door")
-    .setCharacteristic(Characteristic.SerialNumber, "1234567890");
+    .setCharacteristic(this.api.hap.Characteristic.Manufacturer, "jvmo")
+    .setCharacteristic(this.api.hap.Characteristic.Model, "Intercom Door")
+    .setCharacteristic(this.api.hap.Characteristic.SerialNumber, "1234567890");
 
-  // create a new switch service for the relay
-  this.switchService = this.accessory.addService(Service.Switch, this.name);
-  this.switchService
-    .getCharacteristic(Characteristic.On) // get the on/off characteristic
-    .on("get", this.getSwitchState.bind(this)) // bind the getter function
-    .on("set", this.setSwitchState.bind(this)); // bind the setter function
+  // create a new lock service for the relay
+  this.lockService = this.accessory.addService(this.api.hap.Service.LockMechanism, this.name);
+  this.lockService
+    .getCharacteristic(this.api.hap.Characteristic.LockCurrentState)
+    .on("get", this.getLockState.bind(this)); // bind the getter function
+  this.lockService
+    .getCharacteristic(this.api.hap.Characteristic.LockTargetState)
+    .on("get", this.getLockState.bind(this)) // bind the getter function
+    .on("set", this.setLockState.bind(this)); // bind the setter function
 
   // create a new contact sensor service for the voltage
-  this.contactService = this.accessory.addService(Service.ContactSensor, this.name);
+  this.contactService = this.accessory.addService(this.api.hap.Service.ContactSensor, this.name);
   this.contactService
-    .getCharacteristic(Characteristic.ContactSensorState) // get the contact sensor state characteristic
+    .getCharacteristic(this.api.hap.Characteristic.ContactSensorState) // get the contact sensor state characteristic
     .on("get", this.getContactState.bind(this)); // bind the getter function
 
   // listen for changes in the voltage pin
@@ -53,7 +53,7 @@ function IntercomDoor(log, config, api) {
       this.log.error(err); // log the error
     } else {
       this.log.info("Voltage changed to " + value); // log the value
-      this.contactService.updateCharacteristic(Characteristic.ContactSensorState, value); // update the contact sensor state
+      this.contactService.updateCharacteristic(this.api.hap.Characteristic.ContactSensorState, value); // update the contact sensor state
       if (value === 1) {
         this.sendNotification("Bell was pressed"); // send a notification if the voltage is high
       }
@@ -61,31 +61,35 @@ function IntercomDoor(log, config, api) {
   });
 }
 
-// the getter function for the switch state
-IntercomDoor.prototype.getSwitchState = function (callback) {
+// the getter function for the lock state
+IntercomDoor.prototype.getLockState = function (callback) {
   // read the value of the relay pin
   this.relay.read((err, value) => {
     if (err) {
       this.log.error(err); // log the error
       callback(err); // return the error
     } else {
-      this.log.info("Switch state is " + value); // log the value
-      callback(null, value); // return the value
+      const lockState = value === 1 ? this.api.hap.Characteristic.LockCurrentState.UNSECURED : this.api.hap.Characteristic.LockCurrentState.SECURED;
+      this.log.info("Lock state is " + lockState); // log the value
+      callback(null, lockState); // return the value
     }
   });
 };
 
-// the setter function for the switch state
-IntercomDoor.prototype.setSwitchState = function (value, callback) {
+// the setter function for the lock state
+IntercomDoor.prototype.setLockState = function (value, callback) {
   // write the value to the relay pin
-  this.relay.write(value, (err) => {
+  this.relay.write(value === this.api.hap.Characteristic.LockTargetState.UNSECURED ? 1 : 0, (err) => {
     if (err) {
       this.log.error(err); // log the error
       callback(err); // return the error
     } else {
-      this.log.info("Switch state set to " + value); // log the value
+      const lockState = value === this.api.hap.Characteristic.LockTargetState.UNSECURED
+        ? this.api.hap.Characteristic.LockCurrentState.UNSECURED
+        : this.api.hap.Characteristic.LockCurrentState.SECURED;
+      this.log.info("Lock state set to " + lockState); // log the value
       callback(); // return success
-      if (value === 1) {
+      if (value === this.api.hap.Characteristic.LockTargetState.UNSECURED) {
         this.sendNotification("Door is open"); // send a notification if the relay is on
       } else {
         this.sendNotification("Door is closed"); // send a notification if the relay is off
@@ -124,5 +128,5 @@ IntercomDoor.prototype.sendNotification = function (message) {
 // the getServices method for the accessory
 IntercomDoor.prototype.getServices = function () {
   // return an array of the services
-  return [this.informationService, this.switchService, this.contactService];
+  return [this.informationService, this.lockService, this.contactService];
 };
