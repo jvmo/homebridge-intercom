04ae20d8ab3eee6d4d0f45072584734596758e01
jvmo <jvmodeveloper@gmail.com>
2024-02-06 03:45:13 -0500

    index.js

--- index.js
+++ index.js
@@ -18,33 +18,36 @@ function IntercomDoor(log, config, api) {
   this.voltagePin = config.voltagePin || 17; // the GPIO pin for the voltage measurement
   this.apiURL = config.apiURL || "http://localhost:8080"; // the URL of the REST API server
 
+  // get the HAP object from the API
+  const { Accessory, Service, Characteristic } = this.api.hap;
+
   // initialize the GPIO pins using the onoff library
   const Gpio = require("onoff").Gpio;
   this.relay = new Gpio(this.relayPin, "out"); // set the relay pin as an output
   this.voltage = new Gpio(this.voltagePin, "in", "both"); // set the voltage pin as an input with both edge detection
 
   // create a new accessory with the information service
-  this.accessory = new this.api.hap.Accessory(this.name, uuid.v4()); // use api.hap.Accessory
-  this.informationService = this.accessory.getService(this.api.hap.Service.AccessoryInformation);
+  this.accessory = new Accessory(this.name, uuid.v4(), Service.LockMechanism); // use LockMechanism service
+  this.informationService = this.accessory.getService(Service.AccessoryInformation);
   this.informationService
-    .setCharacteristic(this.api.hap.Characteristic.Manufacturer, "jvmo")
-    .setCharacteristic(this.api.hap.Characteristic.Model, "Intercom Door")
-    .setCharacteristic(this.api.hap.Characteristic.SerialNumber, "1234567890");
+    .setCharacteristic(Characteristic.Manufacturer, "jvmo")
+    .setCharacteristic(Characteristic.Model, "Intercom Door")
+    .setCharacteristic(Characteristic.SerialNumber, "1234567890");
 
   // create a new lock service for the relay
-  this.lockService = this.accessory.addService(this.api.hap.Service.LockMechanism, this.name);
+  this.lockService = this.accessory.addService(Service.LockMechanism, this.name);
   this.lockService
-    .getCharacteristic(this.api.hap.Characteristic.LockCurrentState)
+    .getCharacteristic(Characteristic.LockCurrentState)
     .on("get", this.getLockState.bind(this)); // bind the getter function
   this.lockService
-    .getCharacteristic(this.api.hap.Characteristic.LockTargetState)
+    .getCharacteristic(Characteristic.LockTargetState)
     .on("get", this.getLockState.bind(this)) // bind the getter function
     .on("set", this.setLockState.bind(this)); // bind the setter function
 
   // create a new contact sensor service for the voltage
-  this.contactService = this.accessory.addService(this.api.hap.Service.ContactSensor, this.name);
+  this.contactService = this.accessory.addService(Service.ContactSensor, this.name);
   this.contactService
-    .getCharacteristic(this.api.hap.Characteristic.ContactSensorState) // get the contact sensor state characteristic
+    .getCharacteristic(Characteristic.ContactSensorState) // get the contact sensor state characteristic
     .on("get", this.getContactState.bind(this)); // bind the getter function
 
   // listen for changes in the voltage pin
@@ -53,7 +56,7 @@ function IntercomDoor(log, config, api) {
       this.log.error(err); // log the error
     } else {
       this.log.info("Voltage changed to " + value); // log the value
-      this.contactService.updateCharacteristic(this.api.hap.Characteristic.ContactSensorState, value); // update the contact sensor state
+      this.contactService.updateCharacteristic(Characteristic.ContactSensorState, value); // update the contact sensor state
       if (value === 1) {
         this.sendNotification("Bell was pressed"); // send a notification if the voltage is high
       }
@@ -69,7 +72,7 @@ IntercomDoor.prototype.getLockState = function (callback) {
       this.log.error(err); // log the error
       callback(err); // return the error
     } else {
-      const lockState = value === 1 ? this.api.hap.Characteristic.LockCurrentState.UNSECURED : this.api.hap.Characteristic.LockCurrentState.SECURED;
+      const lockState = value === 0 ? this.api.hap.Characteristic.LockCurrentState.UNSECURED : this.api.hap.Characteristic.LockCurrentState.SECURED;
       this.log.info("Lock state is " + lockState); // log the value
       callback(null, lockState); // return the value
     }
@@ -78,8 +81,8 @@ IntercomDoor.prototype.getLockState = function (callback) {
 
 // the setter function for the lock state
 IntercomDoor.prototype.setLockState = function (value, callback) {
-  // write the value to the relay pin
-  this.relay.write(value === this.api.hap.Characteristic.LockTargetState.UNSECURED ? 1 : 0, (err) => {
+  // write the inverted value to the relay pin
+  this.relay.write(value === this.api.hap.Characteristic.LockTargetState.UNSECURED ? 0 : 1, (err) => {
     if (err) {
       this.log.error(err); // log the error
       callback(err); // return the error
@@ -91,6 +94,10 @@ IntercomDoor.prototype.setLockState = function (value, callback) {
       callback(); // return success
       if (value === this.api.hap.Characteristic.LockTargetState.UNSECURED) {
         this.sendNotification("Door is open"); // send a notification if the relay is on
+        // Automatically close the door after 2 seconds
+        setTimeout(() => {
+          this.setLockState(this.api.hap.Characteristic.LockTargetState.SECURED, () => {});
+        }, 2000);
       } else {
         this.sendNotification("Door is closed"); // send a notification if the relay is off
       }
